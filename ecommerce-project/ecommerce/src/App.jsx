f8401ed7205c5ba8c1fe89609c7a32fef16c1923
SandhiyaGiri <sandhiyagiri03@gmail.com>
2025-11-06 23:59:25 +0530

    create multiple changes using react-router

--- ecommerce-project/ecommerce/src/App.jsx
+++ ecommerce-project/ecommerce/src/App.jsx
@@ -1,10 +1,14 @@
 import HomePage from './pages/HomePage'
+import {Routes, Route} from 'react-router'
 import './App.css'
 
 function App() {
   return (
     <>
-    <HomePage />
+    <Routes>
+      <Route path= '/' element={<HomePage />} />
+      <Route path= 'checkout-page' element={<div> Test checkout page </div>} />
+    </Routes>
     </>
   )
 }
